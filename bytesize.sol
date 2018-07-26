9a649ba2537ae57ca970e3414db260efca74b203
Munir Safi <munir.safi1@gmail.com>
2018-07-25 22:42:34 -0400

    storage contract created for contract upgradability options, events declared, add/delete in storage contract

--- bytesize.sol
+++ bytesize.sol
@@ -1,11 +1,31 @@
 pragma solidity ^0.4.17;
+import './bytesize_storage.sol';
 
 contract ByteSize {
 
+    // Global Variable Storage
     address owner;
+    ByteSizeStorage byteStorage;
 
-    function ByteSize() public {
+    // Event Triggers
+    event LoanRequested(uint256 loanID);
+    event StartedLoan(uint256 loanID);
+    event CanceledLoan(uint256 loanID);
+    event PaidLoan(uint256 loanID);
+    event CompletedLoan(uint256 loanID);
+
+    function ByteSize(address _byteStorage) public {
         owner = msg.sender;
+        byteStorage = ByteSizeStorage(_byteStorage);
+    }
+
+    function requestLoan(address lender, uint32 length, uint256 amount) public returns(bool) {
+        if(lender == msg.sender || amount == 0 || length < 10) { revert(); return false; }
+
+        uint256 loanID = byteStorage.addLoan(msg.sender, lender, amount, length, 0);
+
+        LoanRequested(loanID);
+        return true;
     }
 
 }
---
+++ bytesize_storage.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.17;
+
+contract ByteSizeStorage {
+
+    struct Loan {
+        address requester;
+        address lender;
+
+        uint256 amount;
+        uint32 length;
+        uint8 status;
+    }
+
+    // Global Variables
+    Loan[] public loans;
+
+    address owner;
+    address byteSize;
+
+    function ByteSizeStorage() internal {
+        owner = msg.sender;
+    }
+
+    modifier isValidated() {
+        if(msg.sender != byteSize) revert();
+        _;
+    }
+
+    function addLoan(address requester, address lender, uint256 amount, uint32 length, uint8 status) public isValidated returns(uint256) {
+        Loan memory newLoan = Loan(requester, lender, amount, length, status);
+        loans.push(newLoan);
+        
+        return loans.length - 1;
+    }
+
+    function removeLoan(uint256 loanID) public returns(bool) {
+        if(loans.length < loanID) { revert(); return false; }
+
+        delete loans[loanID];
+        return true;
+    }
+
+}
