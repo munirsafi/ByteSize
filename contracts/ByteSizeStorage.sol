065cf75bf0263e62a38745f14265524976ad54e7
Munir Safi <munir.safi1@gmail.com>
2018-07-29 22:32:59 -0400

    governance: moved to a seperate contract

--- contracts/ByteSizeStorage.sol
+++ contracts/ByteSizeStorage.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.4.24;
+import "./ByteSizeGovernance.sol";
 
 contract ByteSizeStorage {
 
@@ -11,26 +12,11 @@ contract ByteSizeStorage {
         mapping(bytes32 => bytes) _bytes;
     }
 
-    struct Resolution {
-        uint256 _resolutionID;
-        uint256 _for;
-        uint256 _against;
-        uint256 _abstain;
-
-        string _status;
-        string _description;
-        mapping(address => bool) _castVotes;
-    }
-
     // Global Variables
 
     address public byteSize;
-
     Loan[] internal loans;
 
-    uint256 public totalBoardMembers;
-    mapping(address => bool) public governance;
-    Resolution[] internal resolutions;
 
 
     // CONTRACT OPERATIONS
@@ -39,68 +25,6 @@ contract ByteSizeStorage {
     modifier isValidated() {
         if(msg.sender != byteSize) revert();
         _;
-    }
-
-
-
-    // GOVERNANCE OPERATIONS
-
-    function addBoardMember(address member) public returns(bool) {
-        if(governance[msg.sender] != true) {
-            revert();
-            return false;
-        }
-
-        governance[member] = true;
-        totalBoardMembers++;
-        return true;
-    }
-
-    function removeBoardMember(address member) public returns(bool) {
-        if(governance[msg.sender] != true || msg.sender == member || address(this) == member) {
-            revert();
-            return false;
-        }
-
-        governance[member] = false;
-        totalBoardMembers--;
-        return true;
-    }
-
-    function newResolution(string description) public returns(bool) {
-        if(governance[msg.sender] != true) {
-            revert();
-            return false;
-        }
-
-        Resolution memory newResolution = Resolution(resolutions.length, 0, 0, 0, "In Progress", description);
-        resolutions.push(newResolution);
-        return true;
-    }
-
-    function castVote(uint resolutionID, int decision) public returns(bool) {
-        if(governance[msg.sender] != true || resolutions[resolutionID]._castVotes[msg.sender] == true) {
-            revert();
-            return false;
-        }
-
-        if(decision == 1) {
-            resolutions[resolutionID]._for++;
-            resolutions[resolutionID]._castVotes[msg.sender] = true;
-        } else if(decision == -1) {
-            resolutions[resolutionID]._against++;
-            resolutions[resolutionID]._castVotes[msg.sender] = true;
-        } else if(decision == 0) {
-            resolutions[resolutionID]._abstain++;
-            resolutions[resolutionID]._castVotes[msg.sender] = true;
-        } else {
-            revert();
-            return false;
-        }
-    }
-
-    function tallyVote() public view returns(int) {
-        
     }
 
 
