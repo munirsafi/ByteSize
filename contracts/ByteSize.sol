807abb32c252f05dc1378fe1a80fc191b7c453e6
Munir Safi <munir.safi1@gmail.com>
2019-03-14 18:28:02 -0400

    feat(pay-loan): initial loan payment function added

--- contracts/ByteSize.sol
+++ contracts/ByteSize.sol
@@ -11,13 +11,13 @@ contract ByteSize {
 
     // Event Triggers
     event LoanRequested(uint256 loanID);
-    event StartedLoan(uint256 loanID);
-    event DeniedLoan(uint256 loanID);
-    event CanceledLoan(uint256 loanID);
-    event PaidLoan(uint256 loanID, uint8 status);
-    event CompletedLoan(uint256 loanID);
+    event LoanStarted(uint256 loanID);
+    event LoanDenied(uint256 loanID);
+    event LoanCanceled(uint256 loanID);
+    event LoanPaid(uint256 loanID, uint256 status);
+    event LoanCompleted(uint256 loanID);
 
-    enum Status { REQUESTED, ACCEPTED, DENIED, CANCELED, COMPLETED, COMPLETED_LATE }
+    enum Status { REQUESTED, ACCEPTED, ACTIVE, DENIED, CANCELED, COMPLETED, COMPLETED_LATE }
 
     constructor(address _byteStorage) public {
         byteStorage = ByteSizeStorage(_byteStorage);
@@ -29,20 +29,21 @@ contract ByteSize {
       *      its properties for the provided values
       * @param lender the address of the wallet providing the loan
       * @param amount the number of tokens requested for the loan
-      * @param length the total number of blocks the loan will last for
+      * @param length the length in minutes that the loan should last for
       * @param interest the interest percentage to be applied over the
                         duration of the loan
-      */
+      * @return <uint> the ID of the newly created loan
+    */
     function requestLoan(address lender, uint256 amount, uint32 length, uint256 interest) public returns(uint) {
         require(lender != msg.sender || amount != 0 || length > 10, "Invalid request; missing parameters");
         uint256 loanID = byteStorage.createLoan();
 
         byteStorage.setAddress(keccak256(abi.encodePacked("lender")), lender, loanID);
         byteStorage.setAddress(keccak256(abi.encodePacked("borrower")), msg.sender, loanID);
-        byteStorage.setUint(keccak256(abi.encodePacked("amount")), amount, loanID);
-        byteStorage.setUint(keccak256(abi.encodePacked("length")), length, loanID);
-        byteStorage.setUint(keccak256(abi.encodePacked("interest")), interest, loanID);
-        byteStorage.setUint(keccak256(abi.encodePacked("status")), uint(Status.REQUESTED), loanID);
+        byteStorage.setUint(keccak256(abi.encodePacked("loan_amount")), amount, loanID);
+        byteStorage.setUint(keccak256(abi.encodePacked("loan_length")), length, loanID);
+        byteStorage.setUint(keccak256(abi.encodePacked("loan_interest")), interest, loanID);
+        byteStorage.setUint(keccak256(abi.encodePacked("loan_status")), uint(Status.REQUESTED), loanID);
 
         emit LoanRequested(loanID);
         return loanID;
@@ -51,13 +52,18 @@ contract ByteSize {
     /**
       * @dev if authorized, updates the state of the loan and activates it
       * @param loanID ID of the requested loan
-     */
-    function acceptLoan(uint loanID) public returns(bool) {
-        require(msg.sender == byteStorage.getAddress(loanID, keccak256(abi.encodePacked("lender"))), "Unauthorized access");
+    */
+    function acceptLoan(uint loanID) public payable returns(bool) {
+        require(msg.sender == byteStorage.getAddress(loanID, keccak256(abi.encodePacked("lender"))) && msg.value > 0, "Unauthorized access");
+
+        address payable borrower = address(uint160(byteStorage.getAddress(loanID, keccak256(abi.encodePacked("borrower")))));
+        uint256 loanAmount = byteStorage.getUint(loanID, keccak256(abi.encodePacked("loan_amount")));
 
         if(byteStorage.getUint(loanID, keccak256(abi.encodePacked("status"))) == uint(Status.REQUESTED)) {
             byteStorage.setUint(keccak256(abi.encodePacked("status")), uint(Status.ACCEPTED), loanID);
-            emit StartedLoan(loanID);
+            byteStorage.setUint(keccak256(abi.encodePacked("start_time")), block.timestamp, loanID);
+            borrower.transfer(loanAmount);
+            emit LoanStarted(loanID);
             return true;
         }
 
@@ -67,17 +73,54 @@ contract ByteSize {
     /**
       * @dev allows the lender to deny a loan request made to them
       * @param loanID ID of the requested loan
-     */
+      * @return <boolean> true if the loan was successfully denied, or false if not
+    */
     function denyLoan(uint loanID) public returns(bool) {
         require(msg.sender == byteStorage.getAddress(loanID, keccak256(abi.encodePacked("lender"))), "Error");
 
         if(byteStorage.getUint(loanID, keccak256(abi.encodePacked("status"))) == uint(Status.REQUESTED)) {
             byteStorage.setUint(keccak256(abi.encodePacked("status")), uint(Status.DENIED), loanID);
-            emit DeniedLoan(loanID);
+            byteStorage.setUint(keccak256(abi.encodePacked("end_time")), block.timestamp, loanID);
+            emit LoanDenied(loanID);
             return true;
         }
 
         return false;
+    }
+
+    /**
+      * @dev gives the borrower the ability to pay back towards a certain loan
+      * @param loanID ID of the requested loan
+      * @return <uint> the total of how much has been paid back so far
+    */
+    function payLoan(uint loanID) public payable returns(uint) {
+        require(msg.sender == byteStorage.getAddress(loanID, keccak256(abi.encodePacked("borrower"))) && msg.value > 0, "Error");
+
+        if(byteStorage.getUint(loanID, keccak256(abi.encodePacked("status"))) == uint(Status.ACCEPTED) ||
+            byteStorage.getUint(loanID, keccak256(abi.encodePacked("status"))) == uint(Status.ACTIVE)) {
+
+            uint paidBackSoFar = byteStorage.getUint(loanID, keccak256(abi.encodePacked("paid_back")));
+            uint loanAmount = byteStorage.getUint(loanID, keccak256(abi.encodePacked("loan_amount")));
+            address payable lender = address(uint160(byteStorage.getAddress(loanID, keccak256(abi.encodePacked("lender")))));
+
+            if(paidBackSoFar <= loanAmount) {
+                if(paidBackSoFar + msg.value < loanAmount) {
+                    byteStorage.setUint(keccak256(abi.encodePacked("paid_back")), paidBackSoFar + msg.value, loanID);
+                    lender.transfer(msg.value);
+                    emit LoanPaid(loanID, byteStorage.getUint(loanID, keccak256(abi.encodePacked("status"))));
+                    return paidBackSoFar + msg.value;
+                } else {
+                    byteStorage.setUint(keccak256(abi.encodePacked("paid_back")), paidBackSoFar + msg.value, loanID);
+                    emit LoanCompleted(loanID);
+                    return paidBackSoFar + msg.value;
+                }
+            } else {
+                revert("You are attempting to pay towards a loan that has already been fulfilled");
+            }
+
+        } else {
+            revert("This loan is no longer in an active state.");
+        }
     }
 
 }
